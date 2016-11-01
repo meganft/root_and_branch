57b1cb6ccd7ce8e0110a40c625b2f58a8a30224d
Anna Dolan <anna.dolan@gmail.com>
2016-10-31 21:46:47 -0600

    Add model tests for Item and Category

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a name" do
+        category = Category.new()
+        expect(category).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with a title" do
+        category = Category.new(name: "cats")
+        expect(category).to be_valid
+      end
+    end
+
+    context "uniqueness" do
+      it "has a unique name" do
+        Category.create(name: "Cats")
+        category = Category.new(name: "Cats")
+        expect(category).to be_invalid
+      end
+    end
+  end
+
+  describe "relationships" do
+    it "has many items through item categories" do
+      category = create(:category)
+      expect(category).to respond_to(:items)
+      expect(category).to respond_to(:item_categories)
+    end
+  end
+end
