59eeccbf4325f3bbdd4de321182e67a7bc98c8eb
Megan Talbot <meganftalbot@gmail.com>
2016-11-01 17:31:14 -0600

    Add route for logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,5 +7,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    
+  end
+
+  def destroy
+    session.delete[:user_id]
+    flash[:success] = "You have successfully logged out."
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   root 'sessions#index'
 
   get '/login', to: 'sessions#new'
-  
+  post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
   resources :users, only: [:new, :create, :show]
