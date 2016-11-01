42604f77b7aa385a528aaceefc3e3ecd320c6931
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-10-31 22:27:32 -0600

    Implement start of Category feature
    Create Category show view to display list of all items for sale assigned
    to a given category.
    
    Update routes to include Category show route
    
    Update item_category model to be singular (`belongs_to :item` not items)

--- spec/features/categories/visitor_sees_a_categorys_items_spec.rb
+++ spec/features/categories/visitor_sees_a_categorys_items_spec.rb
@@ -4,11 +4,10 @@ describe "visitor sees a category's items" do
   describe "they visit a category's page" do
     scenario "they see a single category with multiple items" do
       category = create(:category)
-      item1 = category.items.create(title: "title1", description: "describe1", price: 7, image: "image path1", category: category)
-      item2 = category.items.create(title: "title2", description: "describe2", price: 9, image: "image path2", category: category)
+      item1 = category.items.create(title: "title1", description: "describe1", price: 7, image: "image path1")
+      item2 = category.items.create(title: "title2", description: "describe2", price: 9, image: "image path2")
 
       visit category_path(category)
-
       expect(page).to have_content category.name
       expect(page).to have_content item1.title
       expect(page).to have_content item2.title
@@ -16,8 +15,8 @@ describe "visitor sees a category's items" do
 
     scenario "they do not see another category's items" do
       category1, category2 = create_list(:category, 2)
-      item1 = category.items.create(title: "title1", description: "describe1", price: 7, image: "image path1", category: category1)
-      item2 = category.items.create(title: "title2", description: "describe2", price: 9, image: "image path2", category: category2)
+      item1 = category1.items.create(title: "title1", description: "describe1", price: 7, image: "image path1")
+      item2 = category2.items.create(title: "title2", description: "describe2", price: 9, image: "image path2")
 
       visit category_path(category1)
 
