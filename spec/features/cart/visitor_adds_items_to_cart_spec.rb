c6baaba5aa7f5140237b33ff998338e3caeef817
Ali Schlereth <aschlere@sbcglobal.net>
2016-11-01 13:45:08 -0600

    Test multiple items added to cart

--- spec/features/cart/visitor_adds_items_to_cart_spec.rb
+++ spec/features/cart/visitor_adds_items_to_cart_spec.rb
@@ -29,4 +29,24 @@ describe "visitor adds items to cart" do
     expect(page).to_not have_content item.title
     expect(page).to have_content "Total: 0"
   end
+
+  scenario "they add multiple items to a cart" do
+    category = create(:item_on_category)
+    item1, item2 = category.items
+
+    visit category_path(category)
+    within(".item_#{item1.id}") do
+      click_on "Add to cart"
+    end
+    visit category_path(category)
+    within(".item_#{item2.id}") do
+      click_on "Add to cart"
+    end
+    total_price = item1.price + item2.price
+
+    expect(page).to have_content item1.title
+    expect(page).to have_content item2.title
+    expect(page).to have_content "Total: #{total_price}"
+  end
+
 end
