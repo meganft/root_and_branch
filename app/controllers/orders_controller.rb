0f73da8a50dc890ca54469019ad51b7e32226d73
Anna Dolan <anna.dolan@gmail.com>
2016-11-02 15:05:35 -0600

    Add order show page and add functionality to order index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,8 @@ class OrdersController < ApplicationController
   def index
     @orders = current_user.orders.all
   end
+
+  def show
+    @order = Order.find(params[:id])
+  end
 end
--- spec/features/orders/user_can_view_past_orders_spec.rb
+++ spec/features/orders/user_can_view_past_orders_spec.rb
@@ -17,4 +17,28 @@ describe "user visits orders index" do
     expect(page).to have_content(order2.id)
     expect(page).to_not have_content(order3.id)
   end
+
+  scenario "user sees details of past orders" do
+    user = User.create(name: "Bob", email: "cats@cats.cats", password: "cats")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    order = user.orders.create
+
+    visit orders_path
+
+    expect(page).to have_link("Order ##{order.id} placed #{order.created_at.to_date}", href: order_path(order))
+  end
+
+  scenario "user clicks on past order" do
+    user = User.create(name: "Bob", email: "cats@cats.cats", password: "cats")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    order = user.orders.create
+
+    visit orders_path
+
+    click_on("Order ##{order.id} placed #{order.created_at.to_date}")
+
+    expect(current_path).to eq(order_path(order))
+  end
 end
