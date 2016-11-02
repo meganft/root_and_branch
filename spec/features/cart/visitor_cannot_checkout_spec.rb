e2955f2124225630b341ad859a60d1ef0450618f
Anna Dolan <anna.dolan@gmail.com>
2016-11-02 14:00:23 -0600

    Add feature tests for cart

--- spec/features/cart/visitor_cannot_checkout_spec.rb
+++ spec/features/cart/visitor_cannot_checkout_spec.rb
@@ -2,10 +2,42 @@ require 'rails_helper'
 
 describe "a visitor visits their cart" do
   scenario "a visitor sees the cart" do
+    item = Item.create(title: "cat sweater", price: 49.99, description: "beautiful cat sweater", image: "sweater.jpg")
+
+    visit items_path
+
+    click_on "Add to cart"
+
     visit cart_path
 
     expect(page).to_not have_link("Checkout")
 
     expect(page).to have_content("Login or Create Account to Checkout")
+    expect(page).to have_content("cat sweater")
+  end
+
+  scenario "visitor creates an account and sees the cart" do
+    item = Item.create(title: "cat sweater", price: 49.99, description: "beautiful cat sweater", image: "sweater.jpg")
+
+    visit items_path
+
+    click_on "Add to cart"
+
+    visit new_user_path
+
+    fill_in "user[name]", with: "Bob"
+    fill_in "user[email]", with: "cats@cats.com"
+    fill_in "user[password]", with: "cats"
+    click_button "Create Account"
+
+    visit cart_path
+
+    expect(page).to have_content("cat sweater")
+    expect(page).to have_content("Logout")
+
+    click_on "Logout Bob"
+
+    expect(page).to have_content("Login")
+    expect(page).to_not have_content("Logout")
   end
 end
