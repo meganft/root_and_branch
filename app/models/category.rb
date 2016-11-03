dfaa1d0ff599c2b88696d64ceba831f45df3f468
Megan Talbot <meganftalbot@gmail.com>
2016-11-03 10:10:23 -0600

    Add vanity url for category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,15 @@
 class Category < ApplicationRecord
+  attr_accessor :name, :slug
+  
   validates :name, presence: true
   validates :name, uniqueness: true
 
   has_many :item_categories
   has_many :items, through: :item_categories
+
+  validates :slug, presence: true
+
+  def to_param
+    slug
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :items, only: [:index, :show]
-  resources :categories, only: [:show]
+  resources :categories, only: [:show], param: :slug
+  #
+  # get '/grocery', to: 'categories#show'
 
   get '/cart', to: "cart#index"
   post '/cart', to: "cart#create"
@@ -20,4 +22,8 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show]
 
   get '/dashboard', to: 'users#show'
+
+  get "/:slug", :to => "categories#show"
+
+  # map.category_link '/:slug', :controller => 'categories', :action => 'show'
 end
