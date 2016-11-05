2ccc5d851e7a3ae52aacfb5d5d548e09c0dc01cb
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-11-05 15:24:41 -0600

    update admin dash path in test

--- spec/features/admins/admin_can_log_in_spec.rb
+++ spec/features/admins/admin_can_log_in_spec.rb
@@ -9,6 +9,13 @@ describe "admin can log in" do
     fill_in "password", with: admin.password
     click_button "Login"
 
-    expect(current_path).to eq("/admin/dashboard")
+    expect(current_path).to eq(admin_dashboard_path)
+    expect(page).to have_content "Admin Dashboard"
+  end
+
+  scenario "default user cannot see admin dashboard" do
+    uesr = create(:user)
+
+
   end
 end
