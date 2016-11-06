42ae0ed9c53f6b0eb47a8f2c646cc4ed75bbe323
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-11-05 18:32:49 -0600

    Add status filters to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,22 @@
 class Admin::DashboardController < Admin::BaseController
 
   def show
-    @orders = Order.all
     @statuses = Order.count_by_status
+    if params[:status].nil?
+      @orders = Order.all
+    elsif params[:status] == "Ordered"
+      status = Status.find_by(name: "Ordered")
+      @orders = status.orders
+    elsif params[:status] == "Paid"
+      status = Status.find_by(name: "Paid")
+      @orders = status.orders
+    elsif params[:status] == "Cancelled"
+      status = Status.find_by(name: "Cancelled")
+      @orders = status.orders
+    elsif params[:status] == "Completed"
+      status = Status.find_by(name: "Completed")
+      @orders = status.orders
+    end
   end
 
 end
