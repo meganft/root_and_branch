0691e3b30b6d029e0d2d74235d18680513fa47f8
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-11-05 18:06:11 -0600

    Write test for statuses on admin dashboard

--- spec/features/admins/admin_can_see_orders_on_dashboard_spec.rb
+++ spec/features/admins/admin_can_see_orders_on_dashboard_spec.rb
@@ -12,9 +12,27 @@ describe "admin sees list of orders on dashboard" do
     order2.items << item
 
     visit admin_dashboard_path
-    
 
     expect(page).to have_content "Order #{order1.id}"
     expect(page).to have_content "Order #{order2.id}"
+    expect(page).to have_link "Order #{order1.id} placed #{order1.created_at.to_date}", href: order_path(order1)
+    expect(page).to have_link "Order #{order2.id} placed #{order2.created_at.to_date}", href: order_path(order2)
   end
+
+  scenario "admin sees list of statuses and count on dashboard" do
+    admin = create(:admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    item = create(:item)
+    status = Status.create(name: "Ordered")
+    order1 = Order.create(status_id: status.id, user_id: 2)
+    order1.items << item
+    order2 = Order.create(status_id: status.id, user_id: 2)
+    order2.items << item
+
+    visit admin_dashboard_path
+
+    expect(page).to have_content "#{status.name} 2"
+  end
+
 end
