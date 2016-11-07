fe7fb0c4e243918b6d74c35a888f6c56c033806e
Ali Schlereth <aschlere@sbcglobal.net>
2016-11-07 14:01:28 -0700

    Extract dashboard controller logic

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,25 +1,9 @@
 class Admin::DashboardController < Admin::BaseController
+  include DashboardHelper
 
   def show
     @statuses = Status.left_outer_joins(:orders).select('statuses.*, COUNT(orders.*)').group('statuses.id')
-    if params[:status].nil?
-      @orders = Order.all
-    elsif params[:status] == "Ordered"
-      status = Status.find_by(name: "Ordered")
-      @orders = status.orders
-    elsif params[:status] == "Paid"
-      status = Status.find_by(name: "Paid")
-      @orders = status.orders
-    elsif params[:status] == "Cancelled"
-      status = Status.find_by(name: "Cancelled")
-      @orders = status.orders
-    elsif params[:status] == "Completed"
-      status = Status.find_by(name: "Completed")
-      @orders = status.orders
-    else
-      @orders = Order.all
-      flash[:warning] = "Filter does not exist"
-    end
+    filer_orders(params)
   end
 
 end
