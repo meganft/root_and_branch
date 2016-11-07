b5f97f2c8d68083da1ddf7f041d901c43594849a
Ali Schlereth <aschlere@sbcglobal.net>
2016-11-07 14:57:44 -0700

    Add toggle buttons to admin dashboard orders

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -6,4 +6,23 @@ class Admin::DashboardController < Admin::BaseController
     filter_orders(params)
   end
 
+  def update
+    @order = Order.find(params[:order_id])
+    if params[:change_status] == "Cancel"
+      status = Status.find_by(name: "Cancelled")
+    elsif params[:change_status] == "Paid"
+      status = Status.find_by(name: "Paid")
+    elsif params[:change_status] == "Completed"
+      status = Status.find_by(name: "Completed")
+    end
+    if @order.update(status_id: status.id)
+      flash[:success] = "Your order has been updated to be #{status.name}"
+    else
+      flash[:danger] = "Your order did not update."
+    end
+    redirect_to admin_dashboard_path
+  end
+
+
+
 end
