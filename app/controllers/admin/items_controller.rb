61ddf5cce81839a8bdab9bd4cd9813e0a967be92
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-11-07 17:01:44 -0700

    Add edit & update to admin items controller
    Create edit view
    Update test to be functional

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -8,4 +8,21 @@ class Admin::ItemsController < Admin::BaseController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_items_path
+    else
+      render :edit
+    end
+  end
+
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :image, :price, :retired)
+  end
+
 end
--- spec/features/admins/admin_can_edit_existing_items_spec.rb
+++ spec/features/admins/admin_can_edit_existing_items_spec.rb
@@ -11,11 +11,11 @@ describe "admin can edit existing item" do
     click_on "Edit"
 
     expect(current_path).to eq edit_admin_item_path(item)
-    fill_in "title", with: "New title"
-    fill_in "description", with: "new description"
-    fill_in "image", with: "carrots.jpeg"
-    select "False", from: "retired"
-    click_on "Update item"
+    fill_in "item[title]", with: "New title"
+    fill_in "item[description]", with: "new description"
+    fill_in "item[image]", with: "carrots.jpeg"
+    choose "retired"
+    click_on "Update Item"
 
     expect(page).to have_content "New title"
     expect(page).to_not have_content "old title"
